1aa92a365f3e1c3c93920717498f517189c11888
ARMustafa <armcortex24@gmail.com>
2023-11-15 04:50:07 +0500

    second commit

---
+++ src/main.jsx
@@ -0,0 +1,50 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import ErrorPage from "./error-page";
+import EditContact, {
+  action as editAction,
+} from "./routes/edit";
+import Contact, {
+  loader as contactLoader,
+} from "./routes/contact";
+import Root, { 
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
+
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
